ed64becf0e7a64bed38794c65bd2f6413110381a
Afolorunso <folorunso.adesanya@dipolediamond.com>
2019-11-23 22:45:09 +0100

    NGLeadersDB

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,7 +65,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        Route::domain('http://api.ngleadersdb.herokuapp.com')
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
