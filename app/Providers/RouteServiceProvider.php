b2cdf819e450e1776d0ca06e66ac2f89a1803b20
Afolorunso <folorunso.adesanya@dipolediamond.com>
2019-11-23 17:02:54 +0100

    NGLeadersDB Beta -V1.0.1

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,7 +65,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        Route::domain('api.'.parse_url(config('app.url'), PHP_URL_HOST))
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
